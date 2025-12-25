90dae17663d6bd4ae3ad5e1f0aed90709786a11c
X Developer's <working.roshan6.25@gmail.com>
2025-12-25 15:47:08 +0530

    Refactor addProduct and implement deleteProduct functions

--- Server/src/controllers/Product/Add_DeletProduct.js
+++ Server/src/controllers/Product/Add_DeletProduct.js
@@ -7,26 +7,30 @@ import { uploadToCloudinary } from "../../../contrains.js";
 const addProduct = asyncHandler(async (req, res) => {
   console.log(req.body, "addprodut");
   try {
-    const { itemname, itemimg, description } = req.body;
+    const { itemname, description } = req.body;
+      if (!req.file) {
+      throw new ApiError(400, "Product image is required");
+    }
     const imageUrl = await uploadToCloudinary(req.file, "products");
     // 1️⃣ Validate input
     console.log(imageUrl, "cgh");
+  
     if (!imageUrl) {
       return res.status(404).json(new ApiError(400, imageUrl, "Multer Error"));
     }
-    if (!itemname || !itemimg || !description) {
+    if (!itemname  || !description) {
       throw new ApiError(400, "All fields are required");
     }
     console.log(req.user, req.body, "productx");
 
     // 2️⃣ Create product
-    console.log(
+    console.log({
       itemname,
-      itemimg,
+      itemimg : imageUrl,
       description,
-      Activestatus.Active,
-      req.user.userid
-    );
+     status: Activestatus.Active,
+      createdby: req.user.userid,
+  });
     const product = await ProductModel.create({
       itemname,
       itemimg: imageUrl,
@@ -40,10 +44,24 @@ const addProduct = asyncHandler(async (req, res) => {
       .status(201)
       .json(new ApiResponse(201, product, "Product added successfully"));
   } catch (error) {
-    return res
-      .status(404)
-      .json(new ApiError(400, error, "Product added successfully", null));
+     return res
+      .status(error.statusCode || 400)
+      .json(
+        new ApiError(
+          error.statusCode || 400,
+          error.message || "Something went wrong"
+        )
+      );
   }
 });
 
-export { addProduct };
+const deleteProduct=asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    const product=await ProductModel.findByIdAndDelete(id);
+    if(!product){
+        throw new ApiError(400,"Product not found");
+    }
+    return res.status(200).json(new ApiResponse(200,product,"Product deleted successfully"));
+})
+
+export { addProduct, deleteProduct };
